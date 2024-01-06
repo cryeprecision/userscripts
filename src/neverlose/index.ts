0e06048bd66cef344b97ff6efcc781a792a1c3d9
cryeprecision <muench.rico@gmail.com>
2024-01-06 14:14:31 +0100

    working proof of concept

---
+++ src/neverlose/index.ts
@@ -0,0 +1,4 @@
+import { helloWorld } from "../lib";
+
+console.log(`[Neverlose] ${helloWorld()}`);
+console.log(`[Neverlose] ${$("a").attr("href")}`);
--- webpack/base.ts
+++ webpack/base.ts
@@ -1,46 +1,74 @@
 import TerserPlugin from "terser-webpack-plugin";
 import { Configuration, BannerPlugin } from "webpack";
 import { generateHeader } from "../plugins/userscript.plugin";
+import { Dirent, readdirSync } from "fs";
+
+const getProjects = (): Dirent[] => {
+  return readdirSync("./src/", { withFileTypes: true }).filter(
+    (entry) => entry.isDirectory() && entry.name !== "lib"
+  );
+};
+
+type Comment = {
+  value: string;
+  type: "comment1" | "comment2" | "comment3" | "comment4";
+  pos: number;
+  line: number;
+  col: number;
+};
+
+const isUserscriptComment = (_node: unknown, comment: Comment): boolean => {
+  return (
+    comment.type === "comment1" &&
+    /^\s(==UserScript==|==\/UserScript==|@\w+)/.test(comment.value)
+  );
+};
 
 const config: Configuration = {
-    entry: "./src/index.ts",
-    target: "web",
-    resolve: {
-        extensions: [".ts", ".js"],
-    },
-    module: {
-        rules: [
-            {
-                test: /\.m?ts$/,
-                use: "ts-loader",
-                exclude: /node_modules/,
-            },
-        ],
-    },
-    externals: {
-        axios: "axios",
-        "@trim21/gm-fetch": "GM_fetch",
-    },
-    optimization: {
-        minimize: false,
-        minimizer: [new TerserPlugin({
-            // minify: TerserPlugin.swcMinify,
-            terserOptions: {
-                format: {
-                    comments: false,
-                },
-                compress: false,
-                mangle: false,
-            },
-            extractComments: false,
-        })],
-    },
-    plugins: [
-        new BannerPlugin({
-            banner: generateHeader,
-            raw: true,
-        })
-    ]
+  entry: Object.fromEntries(
+    getProjects().map((entry) => [entry.name, `./src/${entry.name}/index.ts`])
+  ),
+  target: "web",
+  resolve: {
+    extensions: [".ts", ".js"],
+  },
+  module: {
+    rules: [
+      {
+        test: /\.m?ts$/,
+        use: "ts-loader",
+        exclude: /node_modules/,
+      },
+    ],
+  },
+  externals: {
+    jquery: "$",
+    dayjs: "dayjs",
+    lodash: "_",
+  },
+  optimization: {
+    minimize: true,
+    minimizer: [
+      new TerserPlugin({
+        minify: TerserPlugin.terserMinify,
+        terserOptions: {
+          format: {
+            comments: isUserscriptComment,
+            semicolons: false,
+          },
+          compress: false,
+          mangle: false,
+        },
+        extractComments: false,
+      }),
+    ],
+  },
+  plugins: [
+    new BannerPlugin({
+      banner: generateHeader,
+      raw: true,
+    }),
+  ],
 };
 
 export default config;
